904d963931eccbeed97a1a9e96c3bad0c4ecc8ff
Max Miller <maxmiller413@gmail.com>
2021-02-10 17:53:16 -0600

    added notes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
-Plant.destroy_all
+#need to have first so you don't add to previous files(dupes)
+Plant.destroy_all 
 Plant.reset_pk_sequence
 
+#passing one large hash
+#use create(calls new & save) > save to save to long term hard drive
 Plant.create(name: "corn tree", bought: "2019-10-20", color: "green")
 Plant.create(name: "inchplant", bought: "20191020", color: "purple and silver")
 Plant.create(name: "prayer plant", bought: "2019-10", color: "green and purple")
